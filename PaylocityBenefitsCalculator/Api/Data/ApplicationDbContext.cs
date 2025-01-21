e33588375682dba7ab4650af44b4096ccf442306
jinu2ID <jinu.jacob90@gmail.com>
2025-01-20 20:32:54 -0500

    Adding sql database for Employees and Dependents

---
+++ PaylocityBenefitsCalculator/Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Dependent> Dependents { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("Employees");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.FirstName).HasMaxLength(100).IsRequired();
+                entity.Property(e => e.LastName).HasMaxLength(100).IsRequired();
+                entity.Property(e => e.Salary).HasColumnType("decimal(10,2)");
+
+                entity.HasMany(e => e.Dependents)
+                    .WithOne()
+                    .HasForeignKey(d => d.EmployeeId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+            modelBuilder.Entity<Dependent>(entity =>
+            {
+                entity.ToTable("Dependents");
+                entity.HasKey(d => d.Id);
+                entity.Property(d => d.FirstName).HasMaxLength(100).IsRequired();
+                entity.Property(d => d.LastName).HasMaxLength(100).IsRequired();
+            });
+        }
+    }
+}
---
+++ PaylocityBenefitsCalculator/PaylocityBenefits.Database/Tables/Employees.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employees]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[FirstName] NVARCHAR(100) NOT NULL,
+	[LastName] NVARCHAR(100) NOT NULL,
+	[Salary] DECIMAL(10,2) NOT NULL,
+	[DateOfBirth] DATE NOT NULL
+)
+GO
