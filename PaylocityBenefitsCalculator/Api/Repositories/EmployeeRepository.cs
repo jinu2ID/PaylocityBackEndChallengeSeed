ab66285482e7263bebac98ff5b03b283c8c787fb
jinu2ID <jinu.jacob90@gmail.com>
2025-01-21 00:28:31 -0500

    add data access layer

---
+++ PaylocityBenefitsCalculator/Api/Repositories/EmployeeRepository.cs
@@ -0,0 +1,43 @@
+﻿using Api.Data;
+using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Repositories
+{
+    public class EmployeeRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Employee>> GetAllAsync()
+        {
+            return await _context.Employees
+                .Include(e => e.Dependents)
+                .OrderBy(e => e.Id)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Employee>> GetPagedAsync(int pageIndex, int pageSize)
+        {
+            return await _context.Employees
+                .Include(e => e.Dependents)
+                .AsNoTracking()
+                .OrderBy(e => e.Id)
+                .Skip(pageIndex * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+        }
+
+    public async Task<Employee?> GetByIdAsync(int id)
+        {
+            return await _context.Employees
+                .Include(e => e.Dependents)
+                .FirstOrDefaultAsync(e => e.Id == id);
+        }
+    }
+}
